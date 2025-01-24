133711fad2677876a7d539625d7fae8835c87b13
Toddynator <Toddynator@hotmail.co.uk>
2025-01-24 21:04:12 +0000

    Tentacle SideScrolling Movement

--- Assets/Scripts/TentacleMovement.cs
+++ Assets/Scripts/TentacleMovement.cs
@@ -1,6 +1,24 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class TentacleMovement : MonoBehaviour
 {
-    
+    public float frequency       = 1f;
+    public float amplitude       = 5f;
+    public float phaseOffset     = 0f; // To separate multiple Tentacles, start at different points in the sin wave
+    public float frequencyOffset = 0f; // Speed of the movement
+    public Vector3 startPosition;
+
+    public bool attacking = false; // Stop Movement when attacking
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        float xOffset = amplitude * Mathf.Sin(frequency * Time.time + phaseOffset);
+        transform.position = startPosition + new Vector3(xOffset, 0, 0);
+    }
 }
