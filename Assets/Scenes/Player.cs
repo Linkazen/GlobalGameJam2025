474b0a9ba28cd4ccfb0b9cd3e6522e40bcd43c00
Jonathan Hurst <Jpch2k4@gmail.com>
2025-01-24 20:13:46 +0000

    Added player attacking code

--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -4,23 +4,41 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     InputAction moveAction;
+    InputAction attackAction;
+
     private Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // InputAction inits
         moveAction = InputSystem.actions.FindAction("Move");
+        attackAction = InputSystem.actions.FindAction("Attack");
+
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Movement
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
 
         if (moveAction.IsPressed())
         {
             rb.linearVelocity = moveValue;
         }
+
+        // Handles attacking
+        InputSystem.onActionChange +=
+            (attackAction, change) =>
+            {
+                switch (change)
+                {
+                    case InputActionChange.ActionStarted:
+                        print("shoot");
+                        break;
+                }
+            };
     }
 }
