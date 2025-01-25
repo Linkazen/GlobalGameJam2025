af0c19e3f600d7bd39bf37d7229f78880b6cd4b7
Aronagox <aaronlimjj@gmail.com>
2025-01-25 13:44:27 +0000

    Main menu done with new UI system

--- Assets/UI/MainMenu.cs
+++ Assets/UI/MainMenu.cs
@@ -1,3 +1,4 @@
+using System.Xml.Serialization;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -19,12 +20,46 @@ public class MainMenu : EditorWindow
         // Each editor window contains a root VisualElement object
         VisualElement root = rootVisualElement;
 
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
-        VisualElement label = new Label("Hello World! From C#");
-        root.Add(label);
-
         // Instantiate UXML
         VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
         root.Add(labelFromUXML);
+
+        SetupButtonHandler();
+    }
+
+    private void SetupButtonHandler()
+    {
+        VisualElement root = rootVisualElement;
+
+        var buttons = root.Query<Button>();
+        buttons.ForEach(RegisterHandler);
+    }
+
+    private void RegisterHandler(Button button)
+    {
+        if (button.name == "Options")
+        {
+            button.RegisterCallback<ClickEvent>(OptionsMenu);
+            Debug.Log(button.name);
+
+        }
+        else if(button.name == "Play")
+        {
+            button.RegisterCallback<ClickEvent>(PlayGame);
+            Debug.Log(button.name);
+        }
+
+    }
+
+    private void OptionsMenu(ClickEvent evt)
+    {
+        VisualElement root = rootVisualElement;
+        Debug.Log("Options Button Clicked");
+    }
+
+    private void PlayGame(ClickEvent evt)
+    {
+        VisualElement root = rootVisualElement;
+        Debug.Log("Play Button Clicked");
     }
 }
---
+++ Assets/UI/MainMenuController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    [SerializeField]
+    private VisualTreeAsset m_MainMenu;
+    private Button _playButton;
+    private Button _optionsButton;
+    private Button _quitButton;
+
+    private void OnEnable()
+    {
+        var uiDocument = GetComponent<UIDocument>();
+
+        _playButton = uiDocument.rootVisualElement.Q("play") as Button;
+        _optionsButton = uiDocument.rootVisualElement.Q("options") as Button;
+        _quitButton = uiDocument.rootVisualElement.Q("quit") as Button;
+
+        var buttons = uiDocument.rootVisualElement.Query<Button>();
+        buttons.ForEach(RegisterHandler);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void RegisterHandler(Button button)
+    {
+        if (button.name == "Play")
+        {
+            button.RegisterCallback<ClickEvent>(PrintClickMessage);
+        }
+        else if (button.name == "Options")
+        {
+            button.RegisterCallback<ClickEvent>(PrintClickMessage);
+        }
+        else if (button.name == "Quit")
+        {
+            button.RegisterCallback<ClickEvent>(PrintClickMessage);
+        }
+    }
+
+    private void PrintClickMessage(ClickEvent evt)
+    {
+        Button button = evt.currentTarget as Button;
+        string buttonName = button.name;
+        Debug.Log(buttonName);
+    }
+}
