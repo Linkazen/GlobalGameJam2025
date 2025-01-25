9d4a48ae3c0a3b6da5609c9208fcd097cca1798c
Gontarekt <Toddynator@hotmail.co.uk>
2025-01-25 15:54:25 +0000

    SquidBoss Creates TentacleBehaviour Array

--- Assets/Scripts/SquidBossBehaviour.cs
+++ Assets/Scripts/SquidBossBehaviour.cs
@@ -1,8 +1,38 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+/*
+ NOTE:
+ * Since I couldn't get a serialized array of scripts to work in unity, instead you have to add all the scripts to the boss itself, 
+ * then on start, the script will put them all into an array. The scripts should be disabled!
+ */
 
 public class SquidBossBehaviour : MonoBehaviour
 {
     public int health = 1000;
+
+    private MonoBehaviour[] tentacleBehaviours;
+
+    private void Start()
+    {
+        //// Get all Tentacle Behaviour scripts, EXCLUDE the Squid Boss's Script.
+
+        MonoBehaviour[] scriptComponents = GetComponents<MonoBehaviour>();
+        tentacleBehaviours = new MonoBehaviour[scriptComponents.Length - 1];
+
+        int behavioursIndex = 0;
+        foreach (MonoBehaviour behaviour in scriptComponents)
+        {
+            if (behaviour.GetType().Name != "SquidBossBehaviour")
+            {
+                tentacleBehaviours[behavioursIndex] = behaviour;
+                behavioursIndex++;
+            }
+        }
+
+        ////
+    }
 
     private void Update()
     {
