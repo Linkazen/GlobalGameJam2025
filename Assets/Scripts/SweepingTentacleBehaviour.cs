fdd1d8bbae6cc4c49161e307833e9d6094de3531
Jonathan Hurst <Jpch2k4@gmail.com>
2025-01-25 15:05:34 +0000

    Added sweeping attack

---
+++ Assets/Scripts/SweepingTentacleBehaviour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+
+public class SweepingTentacleBehaviour : MonoBehaviour
+{
+    [Header("Attack Settings")]
+    public bool sweepRight = true;
+    public bool sweepTop = false;
+    [SerializeField] float sweepWindUp = 0.5f;
+    [SerializeField] float sweepSpeed = 20;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = new Vector3(Camera.main.transform.position.x - (sweepRight ? 8 : -8), 
+            (sweepTop ? 2.5f : -2.5f), 0);
+        StartCoroutine(Attack());
+    }
+
+    IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(sweepWindUp);
+        GetComponent<Rigidbody2D>().linearVelocityX = sweepSpeed * (sweepRight ? 1 : -1);
+        yield return new WaitForSeconds(2);
+        GetComponent<Rigidbody2D>().linearVelocityX = 0;
+        print("Attack finished");
+    }
+}
